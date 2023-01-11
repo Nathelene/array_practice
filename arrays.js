992bde6c4edfcdf32c7cb79406c7fd1fc07d74c2
Natalie Baker <Nathelene76@gmail.com>
2023-01-11 14:34:20 -0700

    Call array method

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
-var friends = [amy, rachel,ross,marcel]
-var friendsAge = [20, 19, 43, 7]
-var likesCoffee = [true, true, false, true]
+var friends = [amy, rachel,ross,marcel];
+//Will add an element to the end of the array
+friends.push(monica);
+var friendsAge = [20, 19, 43, 7];
+//will remove the first element from the array
+friendsAge.shift(20);
+var likesCoffee = [true, true, false, true];
+//will remove the last element from an array
+likesCoffee.pop(true);
