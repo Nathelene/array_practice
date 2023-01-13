4f3a97e0d69e3c0fbab89f304be13b2bb9a3890d
Natalie Baker <Nathelene76@gmail.com>
2023-01-12 18:03:47 -0700

    revise syntax

--- arrays.js
+++ arrays.js
@@ -1,12 +1,15 @@
-var friends = [amy, rachel,ross,marcel];
+var friends = ["amy", "rachel","ross","marcel"];
 //Will add an element to the end of the array
-friends.push(monica);
+friends.push("monica");
+console.log(friends);
 var friendsAge = [20, 19, 43, 7];
 //will remove the first element from the array
 friendsAge.shift(20);
+console.log(friendsAge);
 var likesCoffee = [true, true, false, true];
 //will remove the last element from an array
 likesCoffee.pop(true);
+console.log(likesCoffee);
 
 //Index positions start from 0 and go up by one down the line.
  //For ex: In the last array the index positions left to right are 0, 1, 2, 3
