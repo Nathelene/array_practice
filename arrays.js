e0409aa687c26145546459dcb0e0f2789c7487ce
Natalie Baker <nathelene76@gmail.com>
2023-03-02 15:14:02 -0700

    remove argument from shift() and pop()

--- arrays.js
+++ arrays.js
@@ -1,14 +1,21 @@
 var friends = ["amy", "rachel","ross","marcel"];
-//Will add an element to the end of the array
+// .push() Will add an element to the end of the array as well as return the length of the new array.
+// * Requires an argument
 friends.push("monica");
 console.log(friends);
+
+
 var friendsAge = [20, 19, 43, 7];
-//will remove the first element from the array
-friendsAge.shift(20);
+// .shift() will remove the first element from the array and return the removed element.
+// * Does not require an argument
+friendsAge.shift();
 console.log(friendsAge);
+
+
 var likesCoffee = [true, true, false, true];
-//will remove the last element from an array
-likesCoffee.pop(true);
+// .pop() will remove the last element from an array and return that elements value.
+// * Does not require an argument
+likesCoffee.pop();
 console.log(likesCoffee);
 
 //Index positions start from 0 and go up by one down the line.
